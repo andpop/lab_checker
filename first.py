22f3de8af526ff621e55c8b558c2d4053ff8cf50
Andrey Popov <andpop@mail.ru>
2023-01-14 00:25:59 +0300

    Change transliter function

--- first.py
+++ first.py
@@ -1,9 +1,10 @@
 from transliterate import translit
 
-print("Hello")
-with open('302.txt', encoding='utf-8') as source_file:
-    with open('trans.txt', 'w') as translit_file:
-        for name in source_file:
-            translit_name = translit(name, "ru", reversed=True)
-            print(translit_name)
-            translit_file.write(translit_name)
+def make_translit_names_file(source_file: str, target_file: str):
+    with open(source_file, encoding='utf-8') as src_file, \
+            open(target_file, 'w') as translit_file:
+        translit_file.write(translit(src_file.read(), "ru", reversed=True))
+
+
+make_translit_names_file(source_file="302.txt", target_file="trans.txt")
+
