c125741c81cee113f7c4bbeb0afb323b838aa8c8
Andrey Popov <andpop@mail.ru>
2023-01-13 14:39:32 +0300

    Change first.py

--- first.py
+++ first.py
@@ -1,7 +1,9 @@
 from transliterate import translit
 
 print("Hello")
-with open('302.txt', encoding='utf-8') as f:
-    for name in f:
-        translit_name = translit(name, "ru", reversed=True)
-        print(translit_name)
+with open('302.txt', encoding='utf-8') as source_file:
+    with open('trans.txt', 'a') as translit_file:
+        for name in source_file:
+            translit_name = translit(name, "ru", reversed=True)
+            print(translit_name)
+            translit_file.write(translit_name)
